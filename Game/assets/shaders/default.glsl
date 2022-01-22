800a77080a51b1fee11b3785e666060733a29744
Onur Baykal <theaob@gmail.com>
2022-01-22 19:01:16 +0300

    added noise to fragment shader added vao push functions to shader class

--- Game/assets/shaders/default.glsl
+++ Game/assets/shaders/default.glsl
@@ -21,7 +21,10 @@ in vec4 fColor;
 
 out vec4 color;
 
+uniform float uTime;
+
 void main()
 {
-    color = fColor;
+    float noise = fract(sin(dot(fColor.xy, vec2(12.9898, 78.233))) * 43758.5453);
+    color = fColor * noise;
 }
